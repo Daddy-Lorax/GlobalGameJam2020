81df75c66b5a83172ac274a8451a0fcfd38a8595
Allan Manuba <manuba@ualberta.ca>
2020-02-02 00:34:30 -0700

    Implement new UI

--- Assets/Scripts/UI/UIBar.cs
+++ Assets/Scripts/UI/UIBar.cs
@@ -5,19 +5,16 @@ public class UIBar : MonoBehaviour
 {
     public PersistentData persistentData;
 
-    public Text details;
     public Image bar;
 
     public void UpdateBar(float currentValue, float maxValue)
     {
         if (maxValue != 0 && currentValue > 0)
         {
-            details.text = ((int)currentValue).ToString() + " / " + maxValue.ToString();
             bar.fillAmount = currentValue / maxValue;
         }
         else
         {
-            details.text = "";
             bar.fillAmount = 0.0f;
         }
     }
