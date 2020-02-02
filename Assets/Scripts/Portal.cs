c4911c44618d4b3620ea2ebb8c4b4942db2d46d7
Allan Manuba <manuba@ualberta.ca>
2020-02-01 21:04:12 -0700

    Add portal

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Portal nextPortal;
+
+    private bool justEntered;
+
+    private void Start()
+    {
+        justEntered = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!justEntered)
+        {
+            nextPortal.ReceivePlayer(other);
+        }
+    }
+
+    public void ReceivePlayer(Collider2D other)
+    {
+        justEntered = true;
+        other.transform.position = this.transform.position;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        justEntered = false;
+    }
+}
